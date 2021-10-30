ea2b9907a0675f4cb43cfd945c4b046d9be18bd3
UditRoshan <77438185+UditRoshan@users.noreply.github.com>
2021-10-30 09:02:36 +0530

    completed the home page except footer

--- script.js
+++ script.js
@@ -10,3 +10,51 @@ menuIcon.addEventListener('click', ()=>{
         menuIcon.style.transform = "rotateY(180deg)";
     }
 });
+
+const zooHead = document.querySelector('.secondSec>.zooHead');
+const zooHeading = document.querySelector('.secondSec>.zooHead>h1');
+const zooDetail = document.querySelector('.zooDetail');
+const zooBuyBtn = document.querySelector('.secondSec>.zooHead>a');
+
+const museumHead = document.querySelector('.secondSec>.museumHead');
+const museumHeading = document.querySelector('.secondSec>.museumHead>h1');
+const museumDetail = document.querySelector('.museumDetail');
+const museumBuyBtn = document.querySelector('.secondSec>.museumHead>a');
+
+
+zooHeading.addEventListener('click', ()=>{
+    if (zooHeading.style.top == "10%" && zooHeading.style.left == "12%" && zooHeading.style.fontSize == "2rem" && zooDetail.style.opacity == "1" && zooBuyBtn.style.opacity == "1" && zooBuyBtn.style.pointerEvents == "all") {
+        zooHeading.style.top = "50%";
+        zooHeading.style.left = "50%";
+        zooHeading.style.fontSize = "6rem";
+        zooDetail.style.opacity = "0";
+        zooBuyBtn.style.opacity = "0";
+        zooBuyBtn.style.pointerEvents = "none";
+    } else {
+        zooHeading.style.top = "10%";
+        zooHeading.style.left = "12%";
+        zooHeading.style.fontSize = "2rem";
+        zooDetail.style.opacity = "1";
+        zooBuyBtn.style.opacity = "1";
+        zooBuyBtn.style.pointerEvents = "all";
+    }
+});
+
+museumHeading.addEventListener('click', ()=>{
+    if (museumHeading.style.top == "10%" && museumHeading.style.left == "12%" && museumHeading.style.fontSize == "2rem" && museumDetail.style.opacity == "1" && museumBuyBtn.style.opacity == "1" && museumBuyBtn.style.pointerEvents == "all") {
+        museumHeading.style.top = "50%";
+        museumHeading.style.left = "50%";
+        museumHeading.style.fontSize = "6rem";
+        museumDetail.style.opacity = "0";
+        museumBuyBtn.style.opacity = "0";
+        museumBuyBtn.style.pointerEvents = "none";
+    } else {
+        museumHeading.style.top = "10%";
+        museumHeading.style.left = "12%";
+        museumHeading.style.fontSize = "2rem";
+        museumDetail.style.opacity = "1";
+        museumBuyBtn.style.opacity = "1";
+        museumBuyBtn.style.pointerEvents = "all";
+    }
+});
+
